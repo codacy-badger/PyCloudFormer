139d0f817bd6367ae56f049852a0cfcb462a8a81
steynbergd <dirkscgm@gmail.com>
2019-09-03 10:29:45 +0200

    Continued Development: https://github.com/DirksCGM/PyCloudFormer/projects/1#card-25670904

--- setup.py
+++ setup.py
@@ -12,11 +12,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DirksCGM/PyCloudFormer",
-    packages=setuptools.find_packages(),
+    packages=['jinja2', 'yaml', 'setuptools', 'unittest'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3.0",
         "Operating System :: OS Independent",
     ],
 )
-
